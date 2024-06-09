6f4631c195bc1e00ce43001683a2ddd0146fde3d
Merve Bayindir <76398913+mervebayindir@users.noreply.github.com>
2024-06-09 03:10:54 +0300

    RepositoryDesignPattern bitti

---
+++ RepositoryDesignPattern/RepsoitoryDesignPattern.DataAccessLayer/EntityFramework/EfCategoryDal.cs
@@ -0,0 +1,20 @@
+﻿using RepositoryDesignPattern.EntityLayer.Concrete;
+using RepsoitoryDesignPattern.DataAccessLayer.Abstract;
+using RepsoitoryDesignPattern.DataAccessLayer.Concrete;
+using RepsoitoryDesignPattern.DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepsoitoryDesignPattern.DataAccessLayer.EntityFramework
+{
+    public class EfCategoryDal : GenericRepository<Category>, ICategoryDal
+    {
+        public EfCategoryDal(Context context) : base(context)
+        {
+
+        }
+    }
+}
---
+++ RepositoryDesignPattern/RepsoitoryDesignPattern.DataAccessLayer/EntityFramework/EfProductDal.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RepositoryDesignPattern.EntityLayer.Concrete;
+using RepsoitoryDesignPattern.DataAccessLayer.Abstract;
+using RepsoitoryDesignPattern.DataAccessLayer.Concrete;
+using RepsoitoryDesignPattern.DataAccessLayer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepsoitoryDesignPattern.DataAccessLayer.EntityFramework
+{
+    public class EfProductDal : GenericRepository<Product>, IProductDal
+    {
+        private readonly Context _context;
+        public EfProductDal(Context context) : base(context)
+        {
+            _context = context;
+        }
+
+        public List<Product> ProductListWithCategory()
+        {
+            return _context.Products.Include(x => x.Category).ToList();
+        }
+    }
+}
